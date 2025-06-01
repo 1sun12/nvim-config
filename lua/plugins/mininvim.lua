3723714f5fe9c1a6e72bbf7c757240bc30f032cd
1sun12 <regan_sun12@outlook.com>
2025-06-01 06:38:57 -0500

    animations enabled, changed the colors to the Abyss theme from VSCODE

--- lua/plugins/mininvim.lua
+++ lua/plugins/mininvim.lua
@@ -5,16 +5,13 @@ return {
     version = false,  -- Set to false to use the latest version
     branch = "stable", -- Use the stable branch
     config = function()
-      -- You can require and set up specific mini modules here
-      -- For example:
+      -- Basic mini modules setup
       require("mini.pairs").setup({})
       require("mini.comment").setup({})
       require("mini.surround").setup({})
       
-      -- Add any other mini modules you want to use
-      -- Example for mini.files (file explorer)
+      -- Mini files setup
       require("mini.files").setup({
-        -- Your configuration options here
         mappings = {
           close = 'q',
           go_in = 'l',
@@ -23,27 +20,47 @@ return {
         }
       })
 
+      -- Setup indent scope with animation
+      require('mini.indentscope').setup({
+        symbol = '│',
+        options = { try_as_border = true },
+        draw = {
+          animation = function(_, _) return 20 end -- Smooth animation timing
+        }
+      })
+
+      -- Setup minimap
+      require('mini.map').setup({
+        integrations = {
+          require('mini.map').gen_integration.builtin_search(),
+          require('mini.map').gen_integration.diagnostic(),
+        },
+        window = {
+          show_integration_count = false,
+        },
+      })
+
       -- nvim color scheme; your nvim theme!
       require('mini.base16').setup({
-	      palette = {
-		      -- kimbie dark theme from VsCode
-		      base00 = "#221a0f", -- background
-		      base01 = "#423523", -- lighter bg
-		      base02 = "#51412c", -- selection bg
-		      base03 = "#645342", -- comments
-		      base04 = "#7c5021", -- dark fg
-		      base05 = "#d3af86", -- default fg
-		      base06 = "#e3b583", -- light fg
-		      base07 = "#fbebd4", -- light bg
-		      base08 = "#dc3958", -- variables
-		      base09 = "#f79a32", -- ints, bools, cons
-		      base0A = "#f06431", -- classes
-		      base0B = "#889b4a", -- strings
-		      base0C = "#088649", -- reg ex
-		      base0D = "#8ab1b0", -- functions
-		      base0E = "#98676a", -- keywords
-		      base0F = "#b16139", -- deprecated
-	      }
+        palette = {
+          -- VSCode Abyss theme
+          base00 = "#000c18", -- Background
+          base01 = "#0a1721", -- Lighter background
+          base02 = "#122231", -- Selection background
+          base03 = "#1c3448", -- Comments, invisibles
+          base04 = "#384f66", -- Dark foreground
+          base05 = "#7c99b6", -- Default foreground
+          base06 = "#a9bcd1", -- Light foreground
+          base07 = "#d7e3ed", -- Light background
+          base08 = "#f07178", -- Variables, XML tags
+          base09 = "#f78c6c", -- Integers, Boolean, Constants
+          base0A = "#ffcb6b", -- Classes, Markup Bold
+          base0B = "#c3e88d", -- Strings
+          base0C = "#89ddff", -- Support, Regular Expressions
+          base0D = "#82aaff", -- Functions, Methods
+          base0E = "#c792ea", -- Keywords, Storage
+          base0F = "#ff5370", -- Deprecated, Opening/Closing Embedded Language Tags
+        }
       })
       
       -- Example for mini.statusline
